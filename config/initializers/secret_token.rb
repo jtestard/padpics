45015d446a6fcde979f0aea2d8d60a0ead111843
Jules Testard <julestestard@MacBook-Pro-de-Jules.local>
2013-07-05 10:38:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Padpics::Application.config.secret_token = 'df6ac0562b2dc8ee11f8c4c5dda8a6091aefe2791638833309d467cbb29d7aece6fb3073c4f97b73f656a5a799e9f75e7aa1389812171ced7c3ea047c01cd10e'
